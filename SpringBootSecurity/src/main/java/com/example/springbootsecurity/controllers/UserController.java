97ee2b1ea1a7541c22b9f46d0f8e29b492d531b9
rahman <margub.9363@gmail.com>
2023-02-11 23:57:48 +0530

    till 36:45 Added Methods in UserController

--- SpringBootSecurity/src/main/java/com/example/springbootsecurity/controllers/UserController.java
+++ SpringBootSecurity/src/main/java/com/example/springbootsecurity/controllers/UserController.java
@@ -1,4 +1,35 @@
 package com.example.springbootsecurity.controllers;
 
+import com.example.springbootsecurity.models.User;
+import com.example.springbootsecurity.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+//    all users
+    @GetMapping("/")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+//    return single user
+    @GetMapping("/{userName}")
+    public User getUser(@PathVariable("userName") String userName) {
+        return this.userService.getUser(userName);
+    }
+
+//    adding a user
+    @PostMapping("/")
+    public User addUser(@RequestBody User user){
+        this.addUser(user);
+        return user;
+    }
 }
