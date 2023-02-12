daef53c593079d760d3e0ea331d60ccab57edfd0
rahman <margub.9363@gmail.com>
2023-02-12 15:01:56 +0530

    till 1:22:00 Skipping Authentication

--- SpringBootSecurity/src/main/java/com/example/springbootsecurity/config/MySecurityConfig.java
+++ SpringBootSecurity/src/main/java/com/example/springbootsecurity/config/MySecurityConfig.java
@@ -5,8 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -56,4 +55,10 @@ public class MySecurityConfig {
         return new BCryptPasswordEncoder(10);
     }
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/home");
+    }
+
 }
+
