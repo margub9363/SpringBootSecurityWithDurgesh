d22aa9396e708c375117df002fc1809d4aefbc75
rahman <margub.9363@gmail.com>
2023-02-12 16:33:41 +0530

    Disabling CSRF token

--- SpringBootSecurity/src/main/java/com/example/springbootsecurity/config/MySecurityConfig.java
+++ SpringBootSecurity/src/main/java/com/example/springbootsecurity/config/MySecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -20,7 +21,9 @@ public class MySecurityConfig {
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
         http
-                .csrf().disable()
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
+                .csrf().disable() //We are disabling CSRF to succs post calls
                 .authorizeHttpRequests((authz) -> authz
                         .anyRequest()
                         .authenticated()
