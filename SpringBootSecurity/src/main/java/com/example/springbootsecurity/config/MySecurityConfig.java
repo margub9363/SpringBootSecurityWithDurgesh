17fcca9ab4b79f9962d7d4577725cbd24c5b6d5a
rahman <margub.9363@gmail.com>
2023-02-12 10:29:03 +0530

    till 1:03:08 configured MySecurityConfig for BasicAuth

---
+++ SpringBootSecurity/src/main/java/com/example/springbootsecurity/config/MySecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.springbootsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
