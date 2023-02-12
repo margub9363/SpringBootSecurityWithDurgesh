b9d4d3812974206b4f9e91cdd69d5c5df518185d
rahman <margub.9363@gmail.com>
2023-02-12 12:01:41 +0530

    till 1:15:04 with Password Encoder

--- SpringBootSecurity/src/main/java/com/example/springbootsecurity/config/MySecurityConfig.java
+++ SpringBootSecurity/src/main/java/com/example/springbootsecurity/config/MySecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -29,24 +31,29 @@ public class MySecurityConfig {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        UserDetails user2 = User.withDefaultPasswordEncoder()
-                .username("Rahman")
-                .password("Rahman")
+        UserDetails user = User
+                .withUsername("user")
+                .password(this.passwordEncoder().encode("password"))
+                .roles("USER").build();
+
+        UserDetails user2 = User
+                .withUsername("Rahman")
+                .password(this.passwordEncoder().encode("Rahman"))
                 .roles("USER")
                 .build();
 
-        UserDetails user3 = User.withDefaultPasswordEncoder()
-                .username("Tannu")
-                .password("Tannu")
+        UserDetails user3 = User
+                .withUsername("Tannu")
+                .password(this.passwordEncoder().encode("Tannu"))
                 .roles("USER")
                 .build();
 
         return new InMemoryUserDetailsManager(user,user2,user3);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
     }
 
 }
